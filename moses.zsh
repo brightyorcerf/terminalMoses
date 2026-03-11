aae4c42fbb1b460eccd707b013d3bcc1cf4ad1b9
brightyorcerf <thetejaansh@gmail.com>
2026-03-12 01:01:37 +0530

    24hr

--- moses.zsh
+++ moses.zsh
@@ -3,7 +3,7 @@ MOSES_STAFF="green"
 MOSES_PLAGUE="red"   
 
 preexec() { 
-    print -P "\n%F{$MOSES_STAFF}┏━ %D{%L:%M:%S %p} ━━━━━━━━━━━━━━━━━━━━━━━%f"
+    print -P "\n%F{$MOSES_STAFF}┏━ %D{%H:%M:%S} ━━━━━━━━━━━━━━━━━━━━━━━%f"
     print -P "%F{$MOSES_STAFF}┃%f %F{white}EXECUTING:%f %B$1%b"
 }
 
