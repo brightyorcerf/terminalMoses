271f8601788af841526b6dc0b78f156373af9ca9
brightyorcerf <thetejaansh@gmail.com>
2026-03-12 00:52:49 +0530

    bug

--- install.sh
+++ install.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
-# install.sh - TerminalMoses Interactive Installer
-
 echo "⚓ Welcome to TerminalMoses"
-echo "Choose your staff color (green, magenta, cyan, yellow, white, or a number 0-255):"
+# The < /dev/tty is the secret sauce that makes curl work
+echo "Choose your staff color (green, magenta, cyan, white):"
 read -p ">> " USER_COLOR < /dev/tty
 
-# 1. Download the core script
+# 1. Download
 curl -sL https://raw.githubusercontent.com/brightyorcerf/terminalMoses/main/moses.zsh -o ~/moses.zsh
 
-# 2. Inject the user's color choice
-# The double quotes allow the variable to be swapped into the file
+# 2. Set Color (Mac/Linux compatible)
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    # Mac version
     sed -i '' "s/MOSES_STAFF=\"green\"/MOSES_STAFF=\"$USER_COLOR\"/" ~/moses.zsh
 else
-    # Linux version
     sed -i "s/MOSES_STAFF=\"green\"/MOSES_STAFF=\"$USER_COLOR\"/" ~/moses.zsh
 fi
 
-# 3. Add to .zshrc if not already there
+# 3. Add to .zshrc
 if ! grep -q "source ~/moses.zsh" ~/.zshrc; then
     echo -e "\n# TerminalMoses\nsource ~/moses.zsh" >> ~/.zshrc
 fi
 
-echo "Parting the sea in $USER_COLOR! Restart your terminal or run 'source ~/.zshrc'"
+echo "✅ Parted! Now run: source ~/.zshrc"
